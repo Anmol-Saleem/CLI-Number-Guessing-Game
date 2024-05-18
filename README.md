e67c0c0f87c6a10ccc0af9852644cd72782470b0
ANMOL SALEEM <anmolsaleem661@gmail.com>
2024-05-18 22:35:59 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CLI-Number-Guessing-Game
+ 
---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+// 1) computer will generate a number 
+// 2) user will guess a number
+// 3) compare both numbers
+const random_number = Math.floor(Math.random()*6+1)
+// for user input
+const answer = await inquirer.prompt([
+    {
+    name : "userGuessedNumber",
+    type : "number",
+    message : " Please guess a number between 1-6"
+},
+]);
+
+// for comparing 
+if(answer.userGuessedNumber==random_number){
+    console.log(" Great! You have guessed a right number ")
+}
+else
+{
+    console.log(" Sorry,  wrong number ")
+
+}
